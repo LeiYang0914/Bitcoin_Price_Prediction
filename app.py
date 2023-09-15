4a0dd67640c12fd4fce7c02989bd3c546398e627
LeiYang0914 <86919770+LeiYang0914@users.noreply.github.com>
2023-09-15 18:35:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,26 +19,38 @@ def main():
             font-size: 24px;
             margin: 0;
         }
+        .input-container {
+            margin-bottom: 10px;
+        }
+        .input-container label {
+            font-weight: bold;
+        }
+        .input-container input {
+            width: 100%;
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
     </style>
     """, unsafe_allow_html=True)
 
     st.markdown("<div class='header'><h1>Stock Price Predictor</h1></div>", unsafe_allow_html=True)
 
     # Get user input for 'Open'
-    open_val = st.text_input("Enter Open:")
+    open_val = st.text_input("Enter Open:", value="37599.410156", key="open")
 
     # Get user input for 'High'
-    high = st.text_input("Enter High:")
+    high = st.text_input("Enter High:", value="39478.953125", key="high")
 
     # Get user input for 'Low'
-    low = st.text_input("Enter Low:")
+    low = st.text_input("Enter Low:", value="37243.972656", key="low")
 
     # Get user input for 'Volume'
-    volume = st.text_input("Enter Volume:")
+    volume = st.text_input("Enter Volume:", value="35460750427", key="volume")
 
     if open_val and high and low and volume:
         # Load the model
-        the_best_model = joblib.load('svr_model.pkl')
+        the_best_model = joblib.load('rf_model.pkl')
         scaler = joblib.load('scaler.pkl')
 
         # Prepare user input features
