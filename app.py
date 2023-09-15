0e13724d730ae40a2f345a1a1231406644c2b8fc
LeiYang0914 <86919770+LeiYang0914@users.noreply.github.com>
2023-09-15 16:59:29 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import joblib
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+def main():
+    # Get user input for 'Open', 'High', 'Low', and 'Volume'
+    user_input = st.text_input("Enter Open, High, Low, Volume (comma-separated):")
+    if user_input:
+        open_val, high, low, volume = user_input.split(',')
+
+        # Load the model
+        the_best_model = joblib.load('rf_model.pkl')
+        scaler = joblib.load('scaler.pkl')
+
+        # Prepare user input features
+        user_input_features = np.array([float(open_val), float(high), float(low), float(volume)]).reshape(1, -1)
+
+        # Scale user input features
+        user_input_features = scaler.transform(user_input_features)
+
+        # Make a prediction
+        predicted_close = the_best_model.predict(user_input_features)
+
+        # Print the predicted close price
+        st.write(f"The predicted close price is: {predicted_close[0]:.2f}")
+
+def app():
+    st.title("Stock Price Predictor")
+
+    main()
+
+if __name__ == "__main__":
+    app()
+
+streamlit run app.py
